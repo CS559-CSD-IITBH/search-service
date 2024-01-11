69529ceaab361bb43a1b0057629e05c31e90782f
Sankalp Acharya <sankalpacharya1211@gmail.com>
2024-01-11 21:46:36 +0530

    Changed Go client for ES

--- controllers/elastic.go
+++ controllers/elastic.go
@@ -1,53 +1,92 @@
 package controllers
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	models "github.com/CS559-CSD-IITBH/search-service/models"
+	"github.com/CS559-CSD-IITBH/search-service/models"
+	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/gin-gonic/gin"
-	"gopkg.in/olivere/elastic.v6"
+	"github.com/mitchellh/mapstructure"
 )
 
-func initClient() (*elastic.Client, error) {
-	client, err := elastic.NewClient(elastic.SetURL(os.Getenv("ELASTICSEARCH_URL")))
-	if err != nil {
-		return nil, err
+var esClient *elasticsearch.Client
+
+func init() {
+	config := elasticsearch.Config{
+		Addresses: []string{os.Getenv("ELASTICSEARCH_URL")},
 	}
 
-	return client, nil
+	es, err := elasticsearch.NewClient(config)
+	if err != nil {
+		log.Fatalf("Error creating Elasticsearch client: %s", err)
+	}
+
+	esClient = es
 }
 
 func ElasticQuery(term string) ([]models.Store, error) {
-	client, err := initClient()
-	if err != nil {
-		log.Printf("Error creating Elasticsearch client: %v", err)
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"match": map[string]interface{}{
+				"Items.Name": term,
+			},
+		},
+	}
+
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
 		return nil, err
 	}
 
-	nestedQuery := elastic.NewNestedQuery("Items", elastic.NewMatchQuery("Items.Name", term))
-
-	searchResult, err := client.Search().
-		Index(os.Getenv("MONGODB_COLLECTION")).
-		Query(nestedQuery).
-		Do(context.Background())
+	res, err := esClient.Search(
+		esClient.Search.WithIndex(os.Getenv("ELASTICSEARCH_INDEX")),
+		esClient.Search.WithBody(&buf),
+		esClient.Search.WithContext(context.Background()),
+	)
 
 	if err != nil {
-		log.Printf("Error executing search query: %v", err)
+		log.Printf("Error executing search query: %s", err)
 		return nil, err
 	}
+	defer res.Body.Close()
 
 	var results []models.Store
-	for _, hit := range searchResult.Hits.Hits {
-		var store models.Store
-		err := json.Unmarshal(*hit.Source, &store)
-		if err != nil {
-			log.Printf("Error unmarshaling JSON: %v", err)
+	if res.IsError() {
+		log.Printf("Error response from Elasticsearch: %s", res.String())
+		return nil, fmt.Errorf("error response from Elasticsearch")
+	}
+
+	var searchResult map[string]interface{}
+	if err := json.NewDecoder(res.Body).Decode(&searchResult); err != nil {
+		log.Printf("Error parsing the search result: %s", err)
+		return nil, err
+	}
+
+	hits, found := searchResult["hits"].(map[string]interface{})["hits"].([]interface{})
+	if !found {
+		log.Printf("Error getting hits from search result")
+		return nil, fmt.Errorf("error getting hits from search result")
+	}
+
+	for _, hit := range hits {
+		source, found := hit.(map[string]interface{})["_source"]
+		if !found {
+			log.Printf("Error getting source from search result hit")
 			continue
 		}
+
+		store := models.Store{}
+		if err := mapstructure.Decode(source, &store); err != nil {
+			log.Printf("Error decoding source to Store model: %s", err)
+			continue
+		}
+
 		results = append(results, store)
 	}
 
@@ -65,13 +104,5 @@ func SearchHandler(c *gin.Context) {
 		return
 	}
 
-	resultsJSON, err := json.Marshal(results)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Error marshaling search results",
-		})
-		return
-	}
-
-	c.JSON(http.StatusOK, resultsJSON)
+	c.JSON(http.StatusOK, results)
 }
